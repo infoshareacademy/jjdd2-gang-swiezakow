0264ca207dfdd22a165c5518077af56c749b56c0
Natasza Kasznia <figlemigle@gmail.com>
2017-09-06 19:30:37 +0200

    JD2GS-24 Poprawiony ekran powitania

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,42 @@
 import java.util.Scanner;
 
 public class Main {
+
+    public static final String KOMENDA_WYJSCIA = "exit";
+
     public static void main(String[] args) {
-        System.out.println("\n" + "~~wspanialy pomagacz w zakupach internetowych~~");
-        System.out.println("-----------------------------------------------\n\n");
-        System.out.println("1. Wyszukiwanie produktu\n2. Katalog kategorii\n3. Pomoc\n\n\nPodaj numer kategorii:");
+
+        System.out.println("\n~~wspanialy pomagacz w zakupach internetowych~~");
+        System.out.println("-----------------------------------------------\n");
+        System.out.println("1. Polecenie nr 1");
+        System.out.println("2. Polecenie nr 2");
+        System.out.println("3. Polecenie nr 3");
+        System.out.println("4. Polecenie nr 4");
+        System.out.println();
+        System.out.println("Aby wyjść wybierz: exit");
+        System.out.println();
+        System.out.println("Podaj numer polecenia: ");
+
         Scanner odczyt = new Scanner(System.in);
+        String linia;
+        while ( !(linia = odczyt.nextLine()).equals(KOMENDA_WYJSCIA) ) {
 
-        while (odczyt.hasNextInt()) {
-            int liczba = odczyt.nextInt();
-            switch (liczba) {
-                case (1):
-                    // wejscie do kategorii
+            switch (linia) {
+                case ("1"):
+                    // wejscie do polecenia 1
                     break;
-                case (2):
-                    // wejście do kategorii
+                case ("2"):
+                    // wejście do polcenia 2
                     break;
-                case (3):
-                    // wejście do kategorii
+                case ("3"):
+                    // wejście do polecenia 3
+                    break;
+                case("4"):
+                    // wejście do polecenia 4
+                    break;
                 default:
-                    System.out.print("Niepoprawny numer. Podaj liczbę.");
+                    System.out.println("Niepoprawny numer. Podaj liczbę.");
             }
-
-
         }
     }
 }
