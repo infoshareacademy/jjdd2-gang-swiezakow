84c6fa257b9f5e0ac1626a86ce25d107b6ebd2b6
Ewa J <mizancewa@gmail.com>
2017-09-07 15:31:25 +0200

    nowe foldery

---
+++ src/main/java/pl/infoshareacademy/jjdd2_gang_swiezakow/App.java
@@ -0,0 +1,10 @@
+package pl.infoshareacademy.jjdd2_gang_swiezakow;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
