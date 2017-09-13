1d166115dd2cacad9b5f2460584e0791292c0c7c
bogumilPiotrowski <bogumilp98@gmail.com>
2017-09-13 23:42:13 +0200

    Category picker

--- src/main/java/pl/infoshareacademy/CategoryPickerCommand.java
+++ src/main/java/pl/infoshareacademy/CategoryPickerCommand.java
@@ -1,4 +1,126 @@
 package pl.infoshareacademy;
 
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
 public class CategoryPickerCommand {
+    AllegroCategoryLoader allegroCategoryLoader = new AllegroCategoryLoader();
+
+    HashMap<Integer, List<AllegroCategory>> allegroCategoryTree = (HashMap<Integer, List<AllegroCategory>>) allegroCategoryLoader.loadCategoryTree();
+
+    Scanner odczyt = new Scanner(System.in);
+
+    ArrayList<Integer> arrayListChildrenCategories = new ArrayList<>();
+
+
+
+    Integer choosenCategory = 0;
+    Integer helper = 0;
+
+
+    public CategoryPickerCommand() throws ParserConfigurationException, SAXException, IOException {
+    }
+
+    public void showCategory0Level() {
+
+        for (int i = 0; i < allegroCategoryTree.get(0).size(); i++) {
+            arrayListChildrenCategories.add(allegroCategoryTree.get(0).get(i).getCatID());
+            System.out.println((allegroCategoryTree.get(0).get(i).getCatPosition()+1) + ". " + allegroCategoryTree.get(0).get(i).toString() +"                    " + allegroCategoryTree.get(0).get(i).getCatID());
+        }
+
+        System.out.println("Co chcesz zrobic?");
+
+        this.whatDoYouWnatToDo();
+
+
+
+    }
+
+    public void showChildrenCategory(){
+
+        for (int i = 0; i < allegroCategoryTree.get(0).size(); i++) {
+             if (choosenCategory == (allegroCategoryTree.get(0).get(i).getCatPosition() + 1)) {
+                helper = allegroCategoryTree.get(0).get(i).getCatID();
+        }
+
+        }
+        for (int i = 0; i < allegroCategoryTree.get(helper).size(); i++) {
+            arrayListChildrenCategories.add(allegroCategoryTree.get(helper).get(i).getCatID());
+            System.out.println((allegroCategoryTree.get(helper).get(i).getCatPosition() + 1) + ". " + allegroCategoryTree.get(helper).get(i).toString() + "                    " + allegroCategoryTree.get(helper).get(i).getCatID());
+        }
+
+        System.out.println("Co chcesz zrobic?");
+
+        this.whatDoYouWnatToDo();
+
+    }
+
+    public Integer categoryMover(){
+        String userInput = odczyt.nextLine();
+        String[] userChoose = userInput.split(" ");
+
+
+        if (userChoose[0].equals(CategoryCommands.ENTER.getCommands())){
+                return Integer.parseInt(userChoose[1]);
+        }
+
+
+        return -1;
+    }
+
+
+    public void whatDoYouWnatToDo(){
+        String what = odczyt.nextLine();
+        String[] userChoose;
+        userChoose = what.split(" ");
+
+        if (userChoose[0].equals(CategoryCommands.ENTER.getCommands())){
+            choosenCategory = Integer.parseInt(userChoose[1]);
+            this.showChildrenCategory();
+        }
+
+        if (userChoose[0].equals(CategoryCommands.GENERATE.getCommands())){
+            System.out.println("Wygenerowany link dla: " + userChoose[1] + ": " + this.allegroCategoryTree.get(helper).get(Integer.parseInt(userChoose[1])-1).getName());
+            //generuj link
+        }
+
+        if (userChoose[0].equals(CategoryCommands.BACK.getCommands())){
+            this.helper = this.allegroCategoryTree.get(helper).get(Integer.parseInt(userChoose[1])).getParent();
+            this.choosenCategory = 1;
+        }
+
+        if (!(userChoose[0].equals(CategoryCommands.BACK.getCommands()) || userChoose[0].equals(CategoryCommands.ENTER.getCommands()) ||
+                userChoose[0].equals(CategoryCommands.GENERATE.getCommands()))) {
+            System.out.println("Niepoprawna komenda");
+        }
+    }
+
+
+}
+
+
+
+enum CategoryCommands {
+    ENTER("enter"), GENERATE("generate"), BACK("back");
+
+    private final String commands;
+
+    CategoryCommands(String commands){
+        this.commands = commands;
+    }
+
+    public String getCommands() {
+        return commands;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
--- src/main/java/pl/infoshareacademy/Main.java
+++ src/main/java/pl/infoshareacademy/Main.java
@@ -13,130 +13,50 @@ public class Main {
     public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
 
 
-        AllegroCategoryLoader allegroCategoryLoader = new AllegroCategoryLoader();
+        System.out.println("\n~~wspanialy pomagacz w zakupach internetowych~~");
+        System.out.println("-----------------------------------------------\n");
+        System.out.println("1. Polecenie nr 1");
+        System.out.println("2. Polecenie nr 2");
+        System.out.println("3. Polecenie nr 3");
+        System.out.println("4. Polecenie nr 4");
+        System.out.println();
+        System.out.println("Aby wyjść wybierz: exit");
+        System.out.println();
+        System.out.println("Podaj numer polecenia: ");
 
-        HashMap<Integer, List<AllegroCategory>> allegroCategoryTree = (HashMap<Integer, List<AllegroCategory>>) allegroCategoryLoader.loadCategoryTree();
-
-
-        Set<Map.Entry<Integer, List<AllegroCategory>>> entries = allegroCategoryTree.entrySet();
-
-//        for (Map.Entry<Integer, List<AllegroCategory>> categoryEntry :
-//                entries) {
-//            System.out.println("Key: " + categoryEntry.getKey() + "\nValue: \n" + categoryEntry.getValue());
-//        }
-
-
-        ArrayList<AllegroCategory> allegroCategoryArrayList = new ArrayList<>();
         Scanner odczyt = new Scanner(System.in);
-        Integer levelOfCategory;
+        String linia;
+        while (!(linia = odczyt.nextLine()).equals(KOMENDA_WYJSCIA)) {
 
-        ArrayList<Integer> arrayListChildrenCategories = new ArrayList<>();
+            switch (linia) {
+                case ("1"):
+                    SearchByQuestionsCommand searchByQuestionsCommand = new SearchByQuestionsCommand();
+                    //searchByQuestionsCommand.//categorySearchByQuestionCommand
+                    // wejscie do polecenia 1
+                    break;
+                case ("2"):
+                    SearchCategoryCommand newSearch = new SearchCategoryCommand();
+                    newSearch.handleCommand(odczyt);
+                    // wejście do polcenia 2
+                    break;
+                case ("3"):
+                    CategoryPickerCommand categoryPickerCommand = new CategoryPickerCommand();
+                    categoryPickerCommand.showCategory0Level();
+                    // wejście do polecenia 3
+                    break;
+                case ("4"):
 
-        Integer helper2 = 0;
-
-        for (int i = 0; i < allegroCategoryTree.get(0).size(); i++) {
-            arrayListChildrenCategories.add(allegroCategoryTree.get(0).get(i).getCatID());
-            System.out.println((allegroCategoryTree.get(0).get(i).getCatPosition()+1) + ". " + allegroCategoryTree.get(0).get(i).toString() +"                    " + allegroCategoryTree.get(0).get(i).getCatID());
-        }
-
-        Integer helper = 1;
-        Integer choosenCategory = odczyt.nextInt();
-        for (int i = 0; i < allegroCategoryTree.get(0).size(); i++) {
-            if (choosenCategory == (allegroCategoryTree.get(0).get(i).getCatPosition() + 1)) {
-                helper = allegroCategoryTree.get(0).get(i).getCatID();
+                    // wejście do polecenia 4
+                    break;
+                case ("5"):
+                    SearchQueryCommand searchQueryCommand = new SearchQueryCommand();
+                    //searchByQuestionsCommand.//categoryQueryCommandMethod
+                    break;
+                // wejście do polecenia 5
+                default:
+                    System.out.println("Niepoprawny numer. Podaj liczbę.");
             }
-
-        }
-
-        for (int i = 0; i < allegroCategoryTree.get(helper).size(); i++) {
-                arrayListChildrenCategories.add(allegroCategoryTree.get(helper).get(i).getCatID());
-                System.out.println((allegroCategoryTree.get(helper).get(i).getCatPosition() + 1) + ". " + allegroCategoryTree.get(helper).get(i).toString() + "                    " + allegroCategoryTree.get(helper).get(i).getCatID());
-        }
-
-        choosenCategory = odczyt.nextInt();
-        for (int i = 0; i < allegroCategoryTree.get(helper).size(); i++) {
-            if (choosenCategory == (allegroCategoryTree.get(helper).get(i).getCatPosition() + 1)) {
-                helper = allegroCategoryTree.get(helper).get(i).getCatID();
-            }
-
-        }
-
-        for (int i = 0; i < allegroCategoryTree.get(helper).size(); i++) {
-            arrayListChildrenCategories.add(allegroCategoryTree.get(helper).get(i).getCatID());
-            System.out.println((allegroCategoryTree.get(helper).get(i).getCatPosition() + 1) + ". " + allegroCategoryTree.get(helper).get(i).toString() + "                    " + allegroCategoryTree.get(helper).get(i).getCatID());
-        }
-
-//        while (true) {
-//            levelOfCategory = odczyt.nextInt();
-//            try {
-//                for (int i = 0; i < allegroCategoryTree.get(levelOfCategory).size(); i++) {
-//                    System.out.println(allegroCategoryTree.get(levelOfCategory).get(i).getCatID() + ". " + allegroCategoryTree.get(levelOfCategory).get(i).toString());
-//                }
-//            } catch (java.lang.NullPointerException e) {
-//                System.out.println("Koniec podkategorii");
-//            }
-//
-//        }
-//        int[] helpTable = int[allegroCategoryTree.get(levelOfCategory).size()]
-
-//        System.out.println(entries.size());
-
-
-
-
-//        for (AllegroCategory allegroCategory :
-//                allegroCategoryTree){
-//            System.out.println(allegroCategoryTree.get(levelOfCategory).get(allegroCategory).toString());
-//        }
-//        System.out.println(allegroCategoryTree.get(0).get(0).toString());
-
-
-
-        //Iterator<AllegroCategory> iterator = allegroCategoryLoa.iterator();
-
-//        System.out.println("\n~~wspanialy pomagacz w zakupach internetowych~~");
-//        System.out.println("-----------------------------------------------\n");
-//        System.out.println("1. Polecenie nr 1");
-//        System.out.println("2. Polecenie nr 2");
-//        System.out.println("3. Polecenie nr 3");
-//        System.out.println("4. Polecenie nr 4");
-//        System.out.println();
-//        System.out.println("Aby wyjść wybierz: exit");
-//        System.out.println();
-//        System.out.println("Podaj numer polecenia: ");
-//
-//        Scanner odczyt = new Scanner(System.in);
-//        String linia;
-//        while ( !(linia = odczyt.nextLine()).equals(KOMENDA_WYJSCIA) ) {
-//
-//            switch (linia) {
-//                case ("1"):
-//                    SearchByQuestionsCommand searchByQuestionsCommand = new SearchByQuestionsCommand();
-//                    //searchByQuestionsCommand.//categorySearchByQuestionCommand
-//                    // wejscie do polecenia 1
-//                    break;
-//                case ("2"):
-//                    SearchCategoryCommand newSearch = new SearchCategoryCommand();
-//                    newSearch.handleCommand(odczyt);
-//                    // wejście do polcenia 2
-//                    break;
-//                case ("3"):
-//                    CategoryPickerCommand categoryPickerCommand = new CategoryPickerCommand();
-//                    //categoryPickerCommand.//categoryPickerCammandMethod
-//                    // wejście do polecenia 3
-//                    break;
-//                case("4"):
-//
-//                    // wejście do polecenia 4
-//                    break;
-//                case ("5"):
-//                    SearchQueryCommand searchQueryCommand = new SearchQueryCommand();
-//                    //searchByQuestionsCommand.//categoryQueryCommandMethod
-//                    break;
-//                    // wejście do polecenia 5
-//                default:
-//                    System.out.println("Niepoprawny numer. Podaj liczbę.");
-//            }
         }
     }
+}
 
