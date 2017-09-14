8f86d66ec6017932bcb6af4480f13b77f9b531e3
Natasza Kasznia <figlemigle@gmail.com>
2017-09-14 21:30:31 +0200

    JD2GS-13 zapętlanie menu w pliku Main, restrukturyzacja

--- src/main/java/pl/infoshareacademy/SearchQueryCommand.java
+++ src/main/java/pl/infoshareacademy/SearchQueryCommand.java
@@ -7,7 +7,17 @@ public class SearchQueryCommand {
 
     private static String FILENAME = "Allegro_cathegories_2016-02-13.xml";
 
-    public void queryCommand(Scanner scanner){
+    public void queryCommand(Scanner scanner) {
+        boolean result = true;
+        while (result) {
+           searchQuery(scanner);
+           result = tryAgain(scanner);
+        }
+        System.out.println();
+        System.out.println("Wracasz do głównego menu");
+    }
+
+    private boolean searchQuery(Scanner scanner){
         System.out.println("Czego szukasz?");
         String line = scanner.nextLine();
 
@@ -18,11 +28,29 @@ public class SearchQueryCommand {
         List<AllegroCategory> categories = searcher.searchCategory(line, allCategories);
 
         AllegroCategory category = searcher.printCategoriesAndLetUserChoose(scanner, categories, allCategories);
-        AllegroCategory parent = searcher.findById(allCategories, category.getParent());
 
-        System.out.println("Aby znaleźć poszukiwany przedmiot użyj poniższego zapytania:");
-        System.out.format("%s %s\n", parent.getName(), category.getName());
-        System.out.println();
-        System.out.println("Wracasz do głównego menu");
+        if (category == null) {
+            System.out.println("Nie znaleziono odpowiedniej kategorii");
+            return true;
+        } else {
+            AllegroCategory parent = searcher.findById(allCategories, category.getParent());
+
+            System.out.println("Aby znaleźć poszukiwany przedmiot użyj poniższego zapytania:");
+            System.out.format("%s %s\n", parent.getName().toLowerCase(), category.getName().toLowerCase());
+            return true;
+        }
+    }
+
+    private boolean tryAgain(Scanner scanner){
+        System.out.println("\nCzy chcesz spróbować ponownie?");
+        String answer = scanner.nextLine();
+        if(answer.toLowerCase().equals("tak")){
+            return true;
+        } else if(answer.toLowerCase().equals("nie")){
+             return false;
+        } else {
+            System.out.println("Niepoprawna odpowiedź, spróbuj jeszcze raz");
+            return tryAgain(scanner);
+        }
     }
 }
