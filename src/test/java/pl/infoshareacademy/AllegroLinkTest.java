c1eb47cef39616c373dddaf65ef114bd1d99d19c
Natasza Kasznia <figlemigle@gmail.com>
2017-09-21 22:10:57 +0200

    JD2GS-49 testy jednostkowe dla AllegroLink

---
+++ src/test/java/pl/infoshareacademy/AllegroLinkTest.java
@@ -0,0 +1,27 @@
+package pl.infoshareacademy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AllegroLinkTest {
+
+    @Test
+    public void testAllegroLink() {
+        String name = "Przykładowa nazwa";
+        int id = 10;
+
+        String result = AllegroLink.makeLink(name, id);
+
+        Assert.assertEquals("https://allegro.pl/kategoria/Przykadowa-nazwa-10", result);
+    }
+
+    @Test
+    public void testAllegroLinkWithEmptyString() {
+        String name = "";
+        int id = 1;
+
+        String result = AllegroLink.makeLink(name, id);
+
+        Assert.assertEquals("https://allegro.pl/kategoria/-1", result);
+    }
+}
