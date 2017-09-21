8620e4478af139d7f57216256d7211c220df1390
Ewa Jablonska <mizancewa@gmail.com>
2017-09-22 00:30:40 +0200

    Dodanie mocka dla katalogu

--- src/main/java/pl/infoshareacademy/Catalog.java
+++ src/main/java/pl/infoshareacademy/Catalog.java
@@ -1,13 +1,24 @@
 package pl.infoshareacademy;
 
-public class Catalog {
+import java.util.List;
 
+public class Catalog {
+    //idToSubcategories - mapa kategoria do podkategorii - jak w SearchByQuestionCommand
+
+    public Catalog() {
+
+    }
 
     public AllegroCategory findCategoryById(int categoryId) {
+        //przydalaby sie mapa id to category
         return null;
     }
 
     public boolean hasSubcategories(int categoryId) {
         return false;
+    }
+
+    public List<AllegroCategory> getSubcategories(int categoryId) {
+        return  null;
     }
 }
