7ee781c35de2cc1aca84acb980dc373e5693a81b
Bogumił Piotrowski <bogumilp98@gmail.com>
2017-09-25 15:46:37 +0200

    Zmiany

--- baseapp/src/main/java/pl/infoshareacademy/CategoryPickerCommand.java
+++ baseapp/src/main/java/pl/infoshareacademy/CategoryPickerCommand.java
@@ -20,22 +20,6 @@ public class CategoryPickerCommand {
     private Integer choosenCategory = 0;
     private Integer helper = 0;
 
-    private Integer getChoosenCategory() {
-        return choosenCategory;
-    }
-
-    private void setChoosenCategory(Integer choosenCategory) {
-        this.choosenCategory = choosenCategory;
-    }
-
-    private Integer getHelper() {
-        return helper;
-    }
-
-    private void setHelper(Integer helper) {
-        this.helper = helper;
-    }
-
     public CategoryPickerCommand() throws ParserConfigurationException, SAXException, IOException {
     }
 
@@ -45,16 +29,16 @@ public class CategoryPickerCommand {
             this.showAllOptions();
 
 
-            for (int i = 0; i < allegroCategoryTree.get(getHelper()).size(); i++) {
-                if (getChoosenCategory() == (allegroCategoryTree.get(getHelper()).get(i).getCatPosition() + 1)) {
-                    setHelper(allegroCategoryTree.get(getHelper()).get(i).getCatID());
-                    choosenCategoryHistory.add(getHelper());
+            for (int i = 0; i < allegroCategoryTree.get(helper).size(); i++) {
+                if (choosenCategory== (allegroCategoryTree.get(helper).get(i).getCatPosition() + 1)) {
+                    helper = allegroCategoryTree.get(helper).get(i).getCatID();
+                    choosenCategoryHistory.add(helper);
                 }
 
             }
 
-            for (int i = 0; i < allegroCategoryTree.get(getHelper()).size(); i++) {
-                System.out.println((allegroCategoryTree.get(getHelper()).get(i).getCatPosition() + 1) + ". " + allegroCategoryTree.get(getHelper()).get(i).toString());
+            for (int i = 0; i < allegroCategoryTree.get(helper).size(); i++) {
+                System.out.println((allegroCategoryTree.get(helper).get(i).getCatPosition() + 1) + ". " + allegroCategoryTree.get(helper).get(i).toString());
             }
             System.out.println("Co chcesz zrobic?");
 
@@ -62,8 +46,8 @@ public class CategoryPickerCommand {
 
         } catch (java.lang.NullPointerException e) {
             System.out.println((char)27 +"[31mBrak podkategorii, możesz wygenerować link lub się cofnąć" + (char)27 +"[0m ");
-            setChoosenCategory(0);
-            setHelper(this.choosenCategoryHistory.get(this.choosenCategoryHistory.size() - 2));
+            choosenCategory = 0;
+            helper = this.choosenCategoryHistory.get(this.choosenCategoryHistory.size() - 2);
             this.choosenCategoryHistory.remove(this.choosenCategoryHistory.size()-1);
             this.showChildrenCategory();
         }
@@ -96,8 +80,8 @@ public class CategoryPickerCommand {
         if (userChoose[0].equals(CategoryCommands.ENTER.getCommands())){
 
             try {
-                if (Integer.parseInt(userChoose[1]) > 0 && Integer.parseInt(userChoose[1]) <= this.allegroCategoryTree.get(getHelper()).size()) {
-                    setChoosenCategory(Integer.parseInt(userChoose[1]));
+                if (Integer.parseInt(userChoose[1]) > 0 && Integer.parseInt(userChoose[1]) <= this.allegroCategoryTree.get(helper).size()) {
+                    choosenCategory = Integer.parseInt(userChoose[1]);
                     this.showChildrenCategory();
                 } else {
                     System.out.println("Brak istniejącej kategorii do której chcesz wejść");
@@ -120,15 +104,15 @@ public class CategoryPickerCommand {
         try {
             if (userChoose[0].equals(CategoryCommands.GENERATE.getCommands())){
 
-                System.out.println("Wygenerowany link dla: " + userChoose[1] + ": " + this.allegroCategoryTree.get(getHelper()).get(Integer.parseInt(userChoose[1])-1).getName());
-                String phraseInLink = this.allegroCategoryTree.get(getHelper()).get(Integer.parseInt(userChoose[1])-1).getName();
+                System.out.println("Wygenerowany link dla: " + userChoose[1] + ": " + this.allegroCategoryTree.get(helper).get(Integer.parseInt(userChoose[1])-1).getName());
+                String phraseInLink = this.allegroCategoryTree.get(helper).get(Integer.parseInt(userChoose[1])-1).getName();
                 phraseInLink = phraseInLink.replace('ą', 'a')
                         .replace('ć', 'c').replace('ę','e')
                         .replace('ł','l').replace('n','n')
                         .replace('ó','o').replace('ś','s')
                         .replace('ź','z').replace('ż','z')
                         .replace(" ", "-");
-                System.out.println("https://allegro.pl/kategoria/" +phraseInLink+ "-" + this.allegroCategoryTree.get(getHelper()).get(Integer.parseInt(userChoose[1])-1).getCatID());
+                System.out.println("https://allegro.pl/kategoria/" +phraseInLink+ "-" + this.allegroCategoryTree.get(helper).get(Integer.parseInt(userChoose[1])-1).getCatID());
                 //generuj link
             }
         } catch (java.lang.IndexOutOfBoundsException e){
@@ -146,16 +130,16 @@ public class CategoryPickerCommand {
         try {
             if (userChoose[0].equals(CategoryCommands.BACK.getCommands())) {
                 if (choosenCategoryHistory.size() == 1) {
-                    this.setHelper(0);
-                    this.setChoosenCategory(0);
+                    this.helper = 0;
+                    this.choosenCategory = 0;
                     this.choosenCategoryHistory.clear();
                     this.showChildrenCategory();
                 } else if (choosenCategoryHistory.isEmpty()) {
                     System.out.println("Przechodzisz do głównego menu");
                 } else {
-                    setHelper(choosenCategoryHistory.get(choosenCategoryHistory.size() - 2));
+                    helper = choosenCategoryHistory.get(choosenCategoryHistory.size() - 2);
                     choosenCategoryHistory.remove(choosenCategoryHistory.size() - 1);
-                    this.setChoosenCategory(0);
+                    this.choosenCategory = 0;
                     this.showChildrenCategory();
                 }
             }
---
+++ baseapp/src/test/java/pl/infoshareacademy/CategoryPickerCommandTest.java
@@ -0,0 +1,6 @@
+package pl.infoshareacademy;
+
+public class CategoryPickerCommandTest {
+
+
+}
