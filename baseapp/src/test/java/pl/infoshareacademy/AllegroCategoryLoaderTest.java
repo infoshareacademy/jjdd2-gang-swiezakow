a6313f5df9c4c73e89ad76901deedf5a4e38e20c
Michał Sefanik <dyziuuu@gmail.com>
2017-09-26 16:19:54 +0200

    deleted empty spaces in AllegroCategoryLoaderTest

--- baseapp/src/test/java/pl/infoshareacademy/AllegroCategoryLoaderTest.java
+++ baseapp/src/test/java/pl/infoshareacademy/AllegroCategoryLoaderTest.java
@@ -24,79 +24,49 @@ import static org.junit.Assert.*;
 public class AllegroCategoryLoaderTest  {
     private static String FILENAME = "test.xml";
     private static String FILENAME2 = "empty.xml";
-
-
-
     private List<AllegroCategory> test1() {      // stworzenie listy
         AllegroCategory allegroCategory = new AllegroCategory(26013, "Antyki i Sztuka", 0, 0);  // dodadnie  1 obiektu
         AllegroCategory allegroCategory2 = new AllegroCategory(98553, "Bilety", 0, 1);
         AllegroCategory allegroCategory3 = new AllegroCategory(64477, "Biuro i Reklama", 0, 2);
         List<AllegroCategory> lista = Arrays.asList(allegroCategory, allegroCategory2, allegroCategory3);        //dodanie do listy 3 obiektow
         return lista;
-
     }
     private Map<Integer, List<AllegroCategory>> mapa1() {
-
         Map<Integer, List<AllegroCategory>> fakeMap = new HashMap<>();
         fakeMap.put(0, test1());
-
         return fakeMap;
     }
     @Test
     public void parseFile() throws Exception {
-
         URL url = getClass().getResource("/"+FILENAME);
         System.out.println(url.getPath());
-
         AllegroCategoryLoader allegroCategoryLoader = new AllegroCategoryLoader();
         List<AllegroCategory> result = allegroCategoryLoader.loadAllCategories(url.getPath());
         List<AllegroCategory> list = test1();
-
-        Assert.assertEquals(list, result);
-
+        assertEquals(list, result);
     }
     @Test
     public void emptyFile() {
         URL url = getClass().getResource("/"+FILENAME2);
         System.out.println(url.getPath());
         AllegroCategoryLoader allegroCategoryLoader = new AllegroCategoryLoader();
-
         List<AllegroCategory> result = allegroCategoryLoader.loadAllCategories(url.getPath());
         List<AllegroCategory> list = new ArrayList<>();
-        Assert.assertEquals(list, result);
-
-
+        assertEquals(list, result);
     }
-
     @Test
     public void noFile() {
         AllegroCategoryLoader allegroCategoryLoader2 = new AllegroCategoryLoader();
         List<AllegroCategory> result2 = allegroCategoryLoader2.loadAllCategories("/non/existing/path");
         assertThat(result2.size(), is(0));
     }
-
-
-
-
-
-
     @Test
     public void theSameMap() {
-
         URL url = getClass().getResource("/"+FILENAME);
         AllegroCategoryLoader aCL = new AllegroCategoryLoader();
         Map<Integer, List<AllegroCategory>> resulat = aCL.loadCategoryTree(url.getPath());
-
-
-        Assert.assertEquals( mapa1(), resulat);
-
-
-
-
-
-
+        assertEquals( mapa1(), resulat);
     }
-
 }
 
 
