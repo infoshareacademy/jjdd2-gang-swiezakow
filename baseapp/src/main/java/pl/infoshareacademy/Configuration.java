249bbb6c0c520889e0aa88c8269a378bddc905e2
ewamj <31629442+ewamj@users.noreply.github.com>
2017-10-29 17:25:31 +0100

    Feature/jd2 gs 80 (#23)
    * FB Login, storing username and password in session, displaying in main menu.
    
    * FB Login for all pages, jsp instead of Jtwig, main page display improved
    
    * SearchByQuestions page display improved
    
    * FB login user authentication added
    
    * FB logout function added
    
    * JD2GS-80 - dodanie panelu logowania do google.com
    
    * JD2GS-80 - dodanie servletu googlelog
    
    * JD2GS-106 - poprawne wylogowanie
    
    * JD2GS-106 - poprawki w jsp
    
    * FB logout redirection added.
    Configuration option for switching between local and production facebook login configuration added
    
    * Code review requests fixed.
    Instructions for enabling FB login on localhost added
    
    * config.json small fixes
    
    * config.json small fixes

--- baseapp/src/main/java/pl/infoshareacademy/Configuration.java
+++ baseapp/src/main/java/pl/infoshareacademy/Configuration.java
@@ -6,6 +6,27 @@ public class Configuration {
     private String linkForSCC;
     private String linkForAL;
     private int minPhraseLength;
+    private String facebookAppId;
+    private String testFacebookAppId;
+    private String facebookAppSecret;
+    private String testFacebookAppSecret;
+    private boolean useTestFB;
+
+    public boolean isUseTestFB() {
+        return useTestFB;
+    }
+
+    public String getTestFacebookAppId() {
+        return testFacebookAppId;
+    }
+
+    public String getFacebookAppSecret() {
+        return facebookAppSecret;
+    }
+
+    public String getTestFacebookAppSecret() {
+        return testFacebookAppSecret;
+    }
 
     public Configuration() { }
 
@@ -28,4 +49,6 @@ public class Configuration {
     public String getFilePath() {
         return filePath;
     }
+
+    public String getFacebookAppId() {return facebookAppId;}
 }
---
+++ webapp/src/main/java/pl/infoshareacademy/webapp/auth/GoogleLoginServlet.java
@@ -0,0 +1,71 @@
+package pl.infoshareacademy.webapp.auth;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.apache.ApacheHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+import static pl.infoshareacademy.webapp.auth.FBAuthServlet.USER_EMAIL;
+import static pl.infoshareacademy.webapp.auth.FBAuthServlet.USER_LOGIN_TYPE;
+import static pl.infoshareacademy.webapp.auth.FBAuthServlet.USER_NAME;
+
+@WebServlet("googlelog")
+public class GoogleLoginServlet extends HttpServlet {
+    Logger logger = LogManager.getLogger(GoogleLoginServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String[] ids = req.getParameterMap().get("id");
+        if (ids == null || ids.length == 0) {
+            logger.info("id was not provided");
+            resp.sendRedirect("fblogin");
+        } else {
+            String id = ids[0];
+            HttpTransport transport = new ApacheHttpTransport();
+            JsonFactory jsonFactory = new JacksonFactory();
+
+            GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+                    .setAudience(Collections.singletonList("372851215939-v2iponke1e57fj4bqagmqsvkkgeu2m9f.apps.googleusercontent.com"))
+                    .build();
+
+            GoogleIdToken idToken = null;
+            try {
+                idToken = verifier.verify(id);
+            } catch (GeneralSecurityException e) {
+                logger.error("cannot verify id", e);
+            }
+            if (idToken != null) {
+                GoogleIdToken.Payload payload = idToken.getPayload();
+
+                String userId = payload.getSubject();
+                logger.info("User ID: " + userId);
+
+                String email = payload.getEmail();
+                String name = (String) payload.get("name");
+                String pictureUrl = (String) payload.get("picture");
+                logger.info("User e-mail: " + email);
+                logger.info("User picture: " + pictureUrl);
+
+                req.getSession().setAttribute(USER_NAME, name);
+                req.getSession().setAttribute(USER_EMAIL, email);
+                req.getSession().setAttribute(USER_LOGIN_TYPE, "google");
+                resp.sendRedirect("main");
+            } else {
+                logger.info("Invalid ID token.");
+            }
+        }
+    }
+}
