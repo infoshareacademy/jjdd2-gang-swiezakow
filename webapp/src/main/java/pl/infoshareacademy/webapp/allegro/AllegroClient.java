be6305780460ad39bcbad1aebd938d7154e10c6f
Michał Sefanik <dyziuuu@gmail.com>
2017-10-30 16:45:12 +0100

    saving file for parsing

--- webapp/src/main/java/pl/infoshareacademy/webapp/allegro/AllegroClient.java
+++ webapp/src/main/java/pl/infoshareacademy/webapp/allegro/AllegroClient.java
@@ -5,17 +5,19 @@ import pl.infoshareacademy.webapp.allegro.api.DoGetCatsDataResponse;
 import pl.infoshareacademy.webapp.allegro.api.ServicePort;
 import pl.infoshareacademy.webapp.allegro.api.ServiceService;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.Part;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import java.io.StringWriter;
+import java.io.*;
 
 public class AllegroClient {
-    private static int COUNTRY=1;
-    private static String KEY="702f4373";
+    private static int COUNTRY = 1;
+    private static String KEY = "702f4373";
 
 
-    public static void main(String[] args) throws JAXBException {
+    public static void main(String[] args) throws JAXBException, IOException {
         ServiceService serviceService = new ServiceService();
         ServicePort servicePort = serviceService.getServicePort();
 
@@ -33,5 +35,30 @@ public class AllegroClient {
         // wyswietli Ci w konsoli cale drzewo
         String xmlString = sw.toString();
         System.out.println(xmlString);
+        String content = xmlString;
+
+
+            FileOutputStream fop = null;
+            File file = new File.
+            fop = new FileOutputStream(file);
+
+            // if file doesnt exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            // get the content in bytes
+            byte[] contentInBytes = content.getBytes();
+
+            fop.write(contentInBytes);
+            fop.flush();
+            fop.close();
+
+            System.out.println("Done");
+
+
+
+
+
     }
 }
