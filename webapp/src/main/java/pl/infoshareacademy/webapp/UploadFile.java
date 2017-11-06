00f57b9cbce317692964c0a4f28a2b81a3b5fcad
ewamj <31629442+ewamj@users.noreply.github.com>
2017-11-06 09:28:16 +0100

    translation remembers parameters (#34)

--- webapp/src/main/java/pl/infoshareacademy/webapp/UploadFile.java
+++ webapp/src/main/java/pl/infoshareacademy/webapp/UploadFile.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/index")
 @MultipartConfig
